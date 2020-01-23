b8424f9fa9ed4370406a17628495458436a1a10b
hweigo <huan1466@purdue.edu>
2020-01-22 22:55:15 -0500

    tune Kp=0.25,Ki=0.08,Kd=0.012. Overshoot but works good

--- src/core.py
+++ src/core.py
@@ -100,9 +100,9 @@ class autonomy(object):
 			## Place code here
                     speed  = 0
                     
-                    kp = 0.2
-                    ki = 0.05
-                    kd = 0.01
+                    kp = 0.25
+                    ki = 0.08
+                    kd = 0.012
                     targetUltr = 0.2
                     errorCurr = 0
                     
